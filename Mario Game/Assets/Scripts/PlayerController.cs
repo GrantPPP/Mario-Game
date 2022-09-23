b1d6b08f510e1da5b654a244adba1f4359931f1b
GrantPPP <111304364+GrantPPP@users.noreply.github.com>
2022-09-23 09:34:45 -0700

    adding prefabs and make spikes kill you

--- Mario Game/Assets/Scripts/PlayerController.cs
+++ Mario Game/Assets/Scripts/PlayerController.cs
@@ -92,4 +92,12 @@ public class PlayerController : MonoBehaviour
 
         theAnimator.SetBool("Grounded", grounded);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+         if(collision.gameObject.tag == "Spike")
+         {
+            Debug.Log("Ouch!");
+         }
+    }
 }
